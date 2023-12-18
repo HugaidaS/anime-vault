ab1ad465d74b952e4c19152f2d35a0b0ff3fea58
Victoria <toriatovawebdev@gmail.com>
2023-12-18 13:25:59 +0400

    Added framer motion and infinite scroll, integrated server actions

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,20 +1,44 @@
-import Image from "next/image";
+'use client'
 
+import Image from "next/image";
+import {useInView} from "react-intersection-observer";
+import React, {useEffect, useState} from "react";
+import {fetchAnime} from "@/app/server/action";
+import AnimeCard from "@/components/AnimeCard";
+
+export type AnimeCard = React.JSX.Element
+
+let page = 2
 function LoadMore() {
+  const {ref, inView} = useInView();
+  const [data, setData] = useState<AnimeCard[]>([]);
+
+  useEffect(() => {
+      if(inView){
+          fetchAnime(page).then((res) => {
+              setData([...data, ...res])
+              page++
+          })
+      }
+  }, [inView, data]);
+
   return (
-    <>
-      <section className="flex justify-center items-center w-full">
-        <div>
-          <Image
-            src="./spinner.svg"
-            alt="spinner"
-            width={56}
-            height={56}
-            className="object-contain"
-          />
-        </div>
-      </section>
-    </>
+      <>
+          <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+              {...data}
+          </section>
+          <section className="flex justify-center items-center w-full">
+              <div ref={ref}>
+                  <Image
+                      src="./spinner.svg"
+                      alt="spinner"
+                      width={56}
+                      height={56}
+                      className="object-contain"
+                  />
+              </div>
+          </section>
+      </>
   );
 }
 
